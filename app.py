a92505e0539f7b30a0f57cc0918af892ab83191e
Senesh-Divyanga <divyangasenesh@gmail.com>
2024-10-17 04:09:55 +0100

    got a input and validate input

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 # get nic number as a user input
 
+nic_number = input("Enter your NIC number: ")
+
 
 # validate user input
+
+if len(nic_number) != 12 and nic_number.is_integer() == False:
+    print("Invalid NIC number")
+else:
+    print("Valid NIC number")
+
 
 
 # identify the born year
